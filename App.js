413d7fd88bba6550b3209d7a12cf80ee0e2a1b15
aria-adib <150343001+aria-adib@users.noreply.github.com>
2024-01-05 18:54:02 +0330

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Calculator from "./component/calculator";
+import "./app.css";
+
+function App() {
+  return (
+    <div className="app">
+  <Calculator />
+  </div>
+  );
+}
+
+export default App;
